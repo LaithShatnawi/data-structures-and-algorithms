1f6028a049812b098f6ce8a56358ecedd4ccde30
LaithShatnawi <laith.sh66@gmail.com>
2023-06-13 00:36:13 +0000

    adding test to linked list

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,60 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-module.exports = LinkedList;
+class Linkedlist {
+    constructor() {
+        this.head = null;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            return this;
+        }
+        let currentNode = this.head;
+        while (currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+        return this;
+    }
+    includes(value) {
+        let currentNode = this.head;
+        while (currentNode?.value) {
+            if (value === currentNode.value) {
+                return true;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        return false;
+    }
+    toString() {
+        let currentNode = this.head;
+        let string = '';
+        while (currentNode?.value) {
+            string = string + `{${currentNode.value}} -> `;
+            currentNode = currentNode.next;
+        }
+        return string + 'NULL';
+    }
+}
+
+const ll = new Linkedlist();
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+ll.insert(4)
+ll.insert(5)
+
+console.log(ll)
+console.log(ll.includes(6))
+console.log(ll.toString())
+
+module.exports = Linkedlist;
